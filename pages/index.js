173fb47a7c57c6f44fd2fb067f878cb882eb3e4b
reza kheradmandi <kheradmandi.partotech@gmail.com>
2024-12-07 17:00:18 +0330

    debugging og logic login

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,25 @@
-import Layout from "@/components/Layout";
 import AuthForm from "@/components/AuthForm";
+import { useEffect, useState } from "react";
+import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  ``
+  const [activeUser, setActiveUser] = useState(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const activeUser = localStorage.getItem('active');
+    if (activeUser === "deactive") {
+      setActiveUser(activeUser);
+    } else {
+      router.push('/ai-chat');
+    }
+  }, [router]);
+
   return (
     <Layout title="Home">
-      <AuthForm />
+      {activeUser && <AuthForm />}
     </Layout>
   );
 }
